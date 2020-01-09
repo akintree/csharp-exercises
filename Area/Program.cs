b185b4b0c457dcf819116f0ba69d1cbbdff6b23f
akintree <56491966+akintree@users.noreply.github.com>
2020-01-08 21:35:36 -0600

    Area
    C# Studio 1

---
+++ Area/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a radius:");
+            double radius = double.Parse(Console.ReadLine());
+                    
+            if (radius <= 0)
+            {
+                while (radius <= 0)
+                {
+                    Console.WriteLine("Error. " + radius + " is not above 0. Please try again:");
+                    radius = double.Parse(Console.ReadLine());
+                }
+
+            } 
+            {
+                Console.WriteLine(radius * radius * 3.14);
+            }       
+            
+            Console.ReadLine();
+        }
+    }
+}
