d60591012de86301d074c056d65e722059b2f53a
akintree <56491966+akintree@users.noreply.github.com>
2020-01-15 20:24:42 -0600

    Counting Characters
    tried doing bonus question 4, ended up commenting it out for later

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace CountingCharacters
 {
@@ -8,13 +9,20 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
-            string test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            //string test = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
            
             Dictionary<string, int> letterCounter = new Dictionary<string, int> { };
             int counter = 1;
             string letter;
             
             string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            //the program coudn't find the file String.txt even though it was in what i believe was the correct folder. may revisit later.
+            //using (StreamReader content = new StreamReader("String.txt"))
+            {
+                //string read = content.ReadToEnd();
+            
+            
 
             Console.WriteLine("Please enter some text:");
             string input = (Console.ReadLine());
@@ -39,6 +47,7 @@ namespace CountingCharacters
             foreach(KeyValuePair<string, int> letterPair in letterCounter)
             {
                 Console.WriteLine(letterPair.Key + " : " + letterPair.Value);
+            }
             }
         }
     }
