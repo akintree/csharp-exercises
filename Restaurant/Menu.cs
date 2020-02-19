a3e0f72ba380e4a4e1cb7ffa948350ef0833e7c5
akintree <56491966+akintree@users.noreply.github.com>
2020-02-19 11:39:04 -0600

    Studio 5 - Restaurant Menu

---
+++ Restaurant/Menu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Menu
+    {
+        public List<MenuItem> items = new List<MenuItem>();
+        public DateTime timeUpdated { get; set; }
+
+    }
+}
---
+++ Restaurant/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class MenuItem
+    {
+        public string category { get; set; }
+        public string description { get; set; }
+        public double price { get; private set; }
+
+        public void isNew()
+        {
+            //determine if the menu item is new and return string "NEW"
+        }
+
+        MenuItem(string Category, string Description, double Price)
+        {
+            Category = category;
+            Description = description;
+            Price = price;
+        }
+    }
+}
